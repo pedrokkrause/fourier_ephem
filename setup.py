4725461ad49549f091ca9602f7f44280bfa53334
PedroKKr <52111108+PedroKKr@users.noreply.github.com>
2023-07-26 12:04:32 -0400

    updated

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="fourier_ephem",
-    version="1.0.0",
-    author="Pedro Kleinschmitt Krause",
-    description="An not-so-accurate ephemeris for the Sun and the Moon using a sum of sines approximation",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    py_modules=["fourier_ephem"],
-    package_dir={'':'src/fourier_ephem'},
-    install_requires=["numpy","datetime"]
+from distutils.core import setup
+setup(
+  name = 'fourier_ephem',
+  packages = ['fourier_ephem'],
+  version = '0.1',
+  license='MIT',
+  description = 'An not-so-accurate ephemeris for the Sun and the Moon using a sum of sines approximation',
+  author = 'Pedro Kleinschmitt Krause',
+  url = 'https://github.com/PedroKKr/fourier_ephem',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['fourier', 'ephemeris', 'astronomy'],
+  install_requires=[
+          'numpy',
+          'datetime',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.x',
+  ],
 )
