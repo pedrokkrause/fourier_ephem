a4488bce2d118dd94a28c8ded8cbc526493dcc5e
PedroKKr <52111108+PedroKKr@users.noreply.github.com>
2023-07-26 12:45:18 -0400

    Updated

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
+
 setup(
   name = 'fourier_ephem',
   packages = ['fourier_ephem'],
-  version = '0.1',
+  version = '1.0.0',
   license='MIT',
   description = 'An not-so-accurate ephemeris for the Sun and the Moon using a sum of sines approximation',
+  long_description=open("README.md", 'r').read(),
+  long_description_content_type='text/markdown',
   author = 'Pedro Kleinschmitt Krause',
   url = 'https://github.com/PedroKKr/fourier_ephem',
   download_url = 'https://github.com/PedroKKr/fourier_ephem/archive/refs/tags/v1.0.0.tar.gz',
@@ -18,6 +21,11 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.x',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
   ],
 )
